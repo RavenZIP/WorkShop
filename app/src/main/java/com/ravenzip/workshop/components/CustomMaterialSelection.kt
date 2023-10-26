41e6dacf18ae904789e5971afb2476a289a761ef
Черных Александр <razor01478920@mail.ru>
2023-10-26 07:46:26 +0300

    Добавил CheckBoxesTree и обработку нажатия

--- app/src/main/java/com/ravenzip/workshop/components/CustomMaterialSelection.kt
+++ app/src/main/java/com/ravenzip/workshop/components/CustomMaterialSelection.kt
@@ -1,6 +1,5 @@
 package com.ravenzip.workshop.components
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,20 +17,21 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 data class ComponentInfo(val isSelected: Boolean, val text: String) {}
 
-@SuppressLint("UnrememberedMutableState")
 @Composable
-fun SimpleSwitch(
+fun Switch(
     isChecked: MutableState<Boolean>,
     title: String,
     titleSize: Int = 18,
@@ -62,7 +62,6 @@ fun SimpleSwitch(
     }
 }
 
-@SuppressLint("UnrememberedMutableState")
 @Composable
 fun RadioGroup(
     width: Float = 0.9f,
@@ -102,25 +101,93 @@ fun CheckBoxes(
     colors: CheckboxColors = CheckboxDefaults.colors()
 ) {
     Column(modifier = Modifier.fillMaxWidth(width)) {
-        list.forEach { item ->
-            Row(
-                modifier =
-                Modifier.fillMaxWidth()
-                    .clip(RoundedCornerShape(10.dp))
-                    .clickable {
-
-                    }
-                    .padding(top = 5.dp, bottom = 5.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Checkbox(
-                    checked = item.isSelected,
-                    onCheckedChange = {  },
-                    enabled = enabled,
-                    colors = colors
-                )
-                Text(text = item.text)
+        list.forEachIndexed { index, item ->
+            GetCheckBox(item = item, enabled = enabled, colors = colors) {
+                list[index] = item.copy(isSelected = !item.isSelected)
             }
         }
+    }
+}
+
+@Composable
+fun CheckBoxesTree(
+    width: Float = 0.9f,
+    triState: MutableState<ToggleableState>,
+    triText: String,
+    list: SnapshotStateList<ComponentInfo>,
+    enabled: Boolean = true,
+    colors: CheckboxColors = CheckboxDefaults.colors()
+) {
+    Column(modifier = Modifier.fillMaxWidth(width)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier =
+                Modifier.fillMaxWidth()
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable { triStateClick(list, triState) }
+                    .padding(top = 5.dp, bottom = 5.dp),
+        ) {
+            TriStateCheckbox(state = triState.value, onClick = { triStateClick(list, triState) })
+            Text(text = triText)
+        }
+        list.forEachIndexed { index, item ->
+            GetCheckBox(item = item, enabled = enabled, colors = colors) {
+                list[index] = item.copy(isSelected = !item.isSelected)
+                getTriState(list, triState)
+            }
+        }
+    }
+}
+
+@Composable
+private fun GetCheckBox(
+    item: ComponentInfo,
+    enabled: Boolean,
+    colors: CheckboxColors,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier =
+            Modifier.fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .clickable { onClick() }
+                .padding(top = 5.dp, bottom = 5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = item.isSelected,
+            onCheckedChange = { onClick() },
+            enabled = enabled,
+            colors = colors
+        )
+        Text(text = item.text)
+    }
+}
+
+private fun getTriState(
+    list: SnapshotStateList<ComponentInfo>,
+    triState: MutableState<ToggleableState>
+) {
+    var activeCheckboxes = 0
+    list.forEach { if (it.isSelected) activeCheckboxes += 1 }
+    triState.value =
+        when (activeCheckboxes) {
+            0 -> ToggleableState.Off
+            list.count() -> ToggleableState.On
+            else -> ToggleableState.Indeterminate
+        }
+}
+
+private fun triStateClick(
+    list: SnapshotStateList<ComponentInfo>,
+    triState: MutableState<ToggleableState>
+) {
+    triState.value =
+        when (triState.value) {
+            ToggleableState.On -> ToggleableState.Off
+            else -> ToggleableState.On
+        }
+    list.indices.forEach { index ->
+        list[index] = list[index].copy(isSelected = triState.value == ToggleableState.On)
     }
 }
--- app/src/main/java/com/ravenzip/workshop/screens/SelectionElementsScreen.kt
+++ app/src/main/java/com/ravenzip/workshop/screens/SelectionElementsScreen.kt
@@ -5,24 +5,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ravenzip.workshop.components.CheckBoxes
+import com.ravenzip.workshop.components.CheckBoxesTree
 import com.ravenzip.workshop.components.ComponentInfo
 import com.ravenzip.workshop.components.RadioGroup
-import com.ravenzip.workshop.components.SimpleSwitch
+import com.ravenzip.workshop.components.Switch
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun SelectionElementsScreen() {
     Column(
-        modifier = Modifier.fillMaxWidth().padding(top = 20.dp),
+        modifier =
+            Modifier.fillMaxWidth().padding(top = 20.dp).verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val isChecked = mutableStateOf(false)
@@ -31,11 +36,19 @@ fun SelectionElementsScreen() {
         val radioButton2 = ComponentInfo(isSelected = false, text = "Вторая")
         val radioButton3 = ComponentInfo(isSelected = false, text = "Третья")
         radioList.addAll(arrayListOf(radioButton1, radioButton2, radioButton3))
+
         val checkBoxList = mutableStateListOf<ComponentInfo>()
         val checkbox1 = ComponentInfo(isSelected = false, text = "Первый")
         val checkbox2 = ComponentInfo(isSelected = false, text = "Второй")
         val checkbox3 = ComponentInfo(isSelected = false, text = "Третий")
         checkBoxList.addAll(arrayListOf(checkbox1, checkbox2, checkbox3))
+
+        val triState = mutableStateOf(ToggleableState.Off)
+        val checkBoxList2 = mutableStateListOf<ComponentInfo>()
+        val checkbox4 = ComponentInfo(isSelected = false, text = "Первый")
+        val checkbox5 = ComponentInfo(isSelected = false, text = "Второй")
+        val checkbox6 = ComponentInfo(isSelected = false, text = "Третий")
+        checkBoxList2.addAll(arrayListOf(checkbox4, checkbox5, checkbox6))
 
         Text(text = "Элементы выбора", fontSize = 30.sp)
         Spacer(modifier = Modifier.padding(top = 50.dp))
@@ -43,7 +56,7 @@ fun SelectionElementsScreen() {
         Text(text = "Switch", fontSize = 25.sp)
         Spacer(modifier = Modifier.padding(top = 20.dp))
 
-        SimpleSwitch(isChecked = isChecked, title = "Заголовок", text = "Описание")
+        Switch(isChecked = isChecked, title = "Заголовок", text = "Описание")
         Spacer(modifier = Modifier.padding(top = 20.dp))
 
         Text(text = "RadioButtons", fontSize = 25.sp)
@@ -56,5 +69,8 @@ fun SelectionElementsScreen() {
         Spacer(modifier = Modifier.padding(top = 20.dp))
 
         CheckBoxes(list = checkBoxList)
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+
+        CheckBoxesTree(triState = triState, triText = "Главный", list = checkBoxList2)
     }
 }
