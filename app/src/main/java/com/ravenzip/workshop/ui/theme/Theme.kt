11c0f6f3eb9378b046868777fa63d76903642080
Черных Александр <razor01478920@mail.ru>
2023-11-06 10:08:22 +0300

    Включил динамические темы

--- app/src/main/java/com/ravenzip/workshop/ui/theme/Theme.kt
+++ app/src/main/java/com/ravenzip/workshop/ui/theme/Theme.kt
@@ -1,14 +1,18 @@
 package com.ravenzip.workshop.ui.theme
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
@@ -46,19 +50,15 @@ private val lightColorScheme =
 fun WorkShopTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    // dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme =
         when {
-            // Динамические темы. Для нормального функционирования нужна еще одна переменная
-            // дополнительно регулирующая вкл\выкл данной фичи
-            //        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            //            val context = LocalContext.current
-            //            if (darkTheme) dynamicDarkColorScheme(context) else
-            // dynamicLightColorScheme(context)
-            //        }
-
+            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            }
             darkTheme -> darkColorScheme
             else -> lightColorScheme
         }
