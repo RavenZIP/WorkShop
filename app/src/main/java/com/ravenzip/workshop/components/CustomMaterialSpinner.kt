7c25eae2513b93fbfccf018a2a2d437383f98677
Черных Александр <razor01478920@mail.ru>
2023-11-11 09:24:28 +0300

    Наработки по спиннеру

--- app/src/main/java/com/ravenzip/workshop/components/CustomMaterialSpinner.kt
+++ app/src/main/java/com/ravenzip/workshop/components/CustomMaterialSpinner.kt
@@ -1,9 +1,60 @@
 package com.ravenzip.workshop.components
 
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
-//TODO спиннер
 @Composable
-fun Spinner(){
+fun Spinner(
+    isLoading: Boolean,
+    isIndeterminate: Boolean = true,
+    containerColors: CardColors = CardDefaults.cardColors()
+) {
+    if (isLoading) {
+        Dialog(onDismissRequest = {}) {
+            Card(
+                modifier = Modifier.size(100.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = containerColors
+            ) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+    }
 
+    @Composable
+    fun DeterminateSpinner(progressValue: Float) {
+        val infiniteTransition = rememberInfiniteTransition(label = "")
+        val progressAnimationValue by
+            infiniteTransition.animateFloat(
+                initialValue = 0.0f,
+                targetValue = progressValue,
+                animationSpec = infiniteRepeatable(animation = tween(900)),
+                label = ""
+            )
+        if ()
+        CircularProgressIndicator(
+            progress = { progressAnimationValue },
+        )
+    }
+
+    /// AlertDialog(onDismissRequest = { /*TODO*/}, properties = DialogProperties()) { Text(text =
+    // "АБОБАА") }
 }
--- app/src/main/java/com/ravenzip/workshop/screens/CustomCardsScreen.kt
+++ app/src/main/java/com/ravenzip/workshop/screens/CustomCardsScreen.kt
@@ -6,20 +6,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ravenzip.workshop.components.BottomAppBar
 import com.ravenzip.workshop.components.InfoCard
+import com.ravenzip.workshop.components.SimpleButton
+import com.ravenzip.workshop.components.Spinner
 import com.ravenzip.workshop.components.TopAppBar
 import com.ravenzip.workshop.data.IconParameters
 import com.ravenzip.workshop.data.TextParameters
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun CustomCardsScreen(navController: NavController) {
+    var isLoading by remember { mutableStateOf(false) }
+    var timer = 0
+    val scope = rememberCoroutineScope()
     Scaffold(
         topBar = { TopAppBar("Карточки", backArrow = true) { navController.popBackStack() } },
         bottomBar = { BottomAppBar(navController = navController, buttonsList = bottomBarButtons) }
@@ -31,15 +46,47 @@ fun CustomCardsScreen(navController: NavController) {
             Spacer(modifier = Modifier.padding(top = 30.dp))
 
             InfoCard(
-                icon = IconParameters(value = Icons.Outlined.Info),
+                icon =
+                    IconParameters(
+                        value = Icons.Outlined.Info,
+                        color = MaterialTheme.colorScheme.primary
+                    ),
                 title = TextParameters(value = "Информационная карточка", size = 18),
                 text =
                     TextParameters(
                         value =
                             "Это карточка, которая содержит в себе иконку, заголовок и описание. Предназначена для предоставления пользователям важной информации",
                         size = 14
+                    ),
+                colors =
+                    CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer
                     )
             )
+
+            Spacer(modifier = Modifier.padding(top = 20.dp))
+
+            SimpleButton(
+                text = TextParameters(value = "Показать спиннер", size = 18),
+                textAlign = TextAlign.Center
+            ) {
+                isLoading = true
+                scope.launch {
+                    while (timer != 3) {
+                        delay(1000)
+                        timer += 1
+                    }
+                    timer = 0
+                    isLoading = false
+                }
+            }
         }
+        Spinner(
+            isLoading,
+            containerColors =
+                CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
+        )
+        DeterminateSpinner()
+
     }
 }
