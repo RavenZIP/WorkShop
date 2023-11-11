efa410bf6759bdd60a07d574d2d10300ec3c9bba
Черных Александр <razor01478920@mail.ru>
2023-11-11 08:19:01 +0300

    Больше цветов у темы

--- app/src/main/java/com/ravenzip/workshop/ui/theme/Color.kt
+++ app/src/main/java/com/ravenzip/workshop/ui/theme/Color.kt
@@ -2,22 +2,34 @@ package com.ravenzip.workshop.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val defaultLightPrimary = Color(0xFF386A20)
+//val defaultLightOnPrimary = Color(0xFFFFFFFF)
+//val defaultLightOnPrimaryContainer = Color(0xFFB8F397)
+//val defaultLightSecondary = Color(0xFF55624C)
+//val defaultLightSecondaryContainer = Color(0xFFD9E7CB)
+//val defaultLightSurface = Color(0xFFF8FAF0)
+//val defaultLightSurfaceContainer = Color(0xFFEDEFE5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
+//Light Theme Colors
 val defaultLightPrimary = Color(0xFF386A20)
 val defaultLightOnPrimary = Color(0xFFFFFFFF)
-val defaultLightOnPrimaryContainer = Color(0xFFB8F397)
+val defaultLightPrimaryContainer = Color(0xFFB8F397)
+val defaultLightOnPrimaryContainer = Color(0xFF042100)
 val defaultLightSecondary = Color(0xFF55624C)
 val defaultLightSecondaryContainer = Color(0xFFD9E7CB)
+val defaultLightOnSecondaryContainer = Color(0xFF131F0D)
+val defaultLightTertiary = Color(0xFF386667)
+val defaultLightTertiaryContainer = Color(0xFFBBEBEB)
+val defaultLightOnTertiaryContainer = Color(0xFF002021)
+val defaultLightError = Color(0xFFBA1A1A)
+val defaultLightErrorContainer = Color(0xFFFFDAD6)
+val defaultLightOnErrorContainer = Color(0xFF410002)
 val defaultLightSurface = Color(0xFFF8FAF0)
-val defaultLightSurfaceContainer = Color(0xFFEDEFE5)
+val defaultLightOnSurface = Color(0xFF1A1C18)
+val defaultLightOnSurfaceVariant = Color(0xFF43493E)
+val defaultLightSurfaceContainer = Color(0xFFEDEFE4)
 
+//Dark Theme Colors
 val defaultDarkPrimary = Color(0xFFe3b434)
 val defaultDarkOnPrimary = Color(0xFF242424)
 val defaultDarkSecondary = Color(0xFF575757)
