bbc2735e22e56fcc943334382cf848508f0a9466
Черных Александр <razor01478920@mail.ru>
2023-11-19 10:26:29 +0300

    Поднятие версии material и compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     //Kotlin
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     //Compose UI
     implementation("androidx.compose.ui:ui")
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-android:1.5.4")
     //Material 3
-    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha11")
     //Navigation
     implementation ("androidx.navigation:navigation-compose:2.7.5")
     //Icons
