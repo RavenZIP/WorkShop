201bbb3ba644ffda1862743c7dfd3b561b2be7ae
Черных Александр <razor01478920@mail.ru>
2024-01-20 12:08:58 +0300

    Поднял версию lifecycle-runtime-ktx и material3

--- workshop/build.gradle.kts
+++ workshop/build.gradle.kts
@@ -51,7 +51,7 @@ publishing {
 dependencies {
     // Kotlin
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     // Compose UI
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-android:1.5.4")
     // Material 3
-    implementation("androidx.compose.material3:material3:1.2.0-beta01")
+    implementation("androidx.compose.material3:material3:1.2.0-beta02")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.6")
     // Icons
