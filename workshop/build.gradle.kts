7c5b56b7955ef86487d34031a9bb1c507e319d37
Черных Александр <razor01478920@mail.ru>
2024-02-13 22:20:47 +0300

    Поднял версии Material 3, UI-Text и Compose-Navigation

--- workshop/build.gradle.kts
+++ workshop/build.gradle.kts
@@ -58,11 +58,11 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.ui:ui-text-android:1.6.0")
+    implementation("androidx.compose.ui:ui-text-android:1.6.1")
     // Material 3
-    implementation("androidx.compose.material3:material3:1.2.0-rc01")
+    implementation("androidx.compose.material3:material3:1.2.0")
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     // Other
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
