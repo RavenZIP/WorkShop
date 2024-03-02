b3e4dc87691ccc5b2357ba0f0f7eea1f805b9f34
Черных Александр <razor01478920@mail.ru>
2024-03-02 20:04:47 +0300

    Добавлена возможность расположить заголовок карточки справа от иконки

--- workshop/src/main/java/com/ravenzip/workshop/components/CustomMaterialCard.kt
+++ workshop/src/main/java/com/ravenzip/workshop/components/CustomMaterialCard.kt
@@ -2,6 +2,7 @@ package com.ravenzip.workshop.components
 
 import androidx.annotation.FloatRange
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,9 +14,11 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ravenzip.workshop.data.IconParameters
@@ -28,7 +31,7 @@ import com.ravenzip.workshop.data.TextParameters
  * @param icon иконка (обязательный)
  * @param title заголовок (обязательный)
  * @param text текст (обязательный)
- * @param shape радиус скругления кнопки (по умолчанию 10.dp, не обязательный)
+ * @param isTitleUnderIcon расположить заголовок снизу иконки (по умолчанию true, не обязательный)
  * @param shape радиус скругления (по умолчанию 10.dp, не обязательный)
  * @param colors цвета карточки (по умолчанию берутся из темы приложения, не обязательный)
  */
@@ -38,6 +41,7 @@ fun InfoCard(
     icon: IconParameters,
     title: TextParameters,
     text: TextParameters,
+    isTitleUnderIcon: Boolean = false,
     shape: Shape = RoundedCornerShape(10.dp),
     colors: CardColors = CardDefaults.cardColors()
 ) {
@@ -48,19 +52,19 @@ fun InfoCard(
         elevation = CardDefaults.cardElevation(0.dp)
     ) {
         Column(modifier = Modifier.padding(15.dp)) {
-            Icon(
-                imageVector = icon.value,
-                contentDescription = icon.description,
-                tint = icon.color ?: colors.contentColor,
-                modifier = Modifier.size(icon.size.dp)
-            )
-            Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(
-                text = title.value,
-                color = title.color ?: colors.contentColor,
-                fontSize = title.size.sp,
-                fontWeight = FontWeight.Medium
-            )
+            if (isTitleUnderIcon) {
+                GetIconAndTitle(icon = icon, title = title, isPaddingTop = true, colors = colors)
+            } else {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    GetIconAndTitle(
+                        icon = icon,
+                        title = title,
+                        isPaddingTop = false,
+                        colors = colors
+                    )
+                }
+            }
+
             Spacer(modifier = Modifier.padding(top = 10.dp))
             Text(
                 modifier = Modifier.fillMaxWidth(0.9f),
@@ -72,4 +76,31 @@ fun InfoCard(
         }
     }
 }
-//TODO ExpandableInfoCard
+
+@Composable
+private fun GetIconAndTitle(
+    icon: IconParameters,
+    title: TextParameters,
+    isPaddingTop: Boolean,
+    colors: CardColors
+) {
+    Icon(
+        imageVector = icon.value,
+        contentDescription = icon.description,
+        tint = icon.color ?: colors.contentColor,
+        modifier = Modifier.size(icon.size.dp)
+    )
+    Spacer(modifier = if (isPaddingTop) Modifier.paddingTop(10.dp) else Modifier.paddingEnd(10.dp))
+    Text(
+        text = title.value,
+        color = title.color ?: colors.contentColor,
+        fontSize = title.size.sp,
+        fontWeight = FontWeight.Medium
+    )
+}
+
+private fun Modifier.paddingTop(value: Dp) = this then (this.padding(top = value))
+
+private fun Modifier.paddingEnd(value: Dp) = this then (this.padding(end = value))
+
+// TODO ExpandableInfoCard
