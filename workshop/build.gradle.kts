539bcfcc0ae2dfa2475096734aab917337e2f2df
Черных Александр <razor01478920@mail.ru>
2024-05-05 22:55:10 +0300

    Обновил версии некоторых используемых библиотек

--- workshop/build.gradle.kts
+++ workshop/build.gradle.kts
@@ -45,25 +45,23 @@ publishing {
             artifactId = "workshop"
             version = "1.5.0"
 
-            afterEvaluate {
-                from(components["release"])
-            }
+            afterEvaluate { from(components["release"]) }
         }
     }
 }
 
 dependencies {
     // Kotlin
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
 
     // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.ui:ui-text-android:1.6.6")
+    implementation("androidx.compose.ui:ui-text-android:1.6.7")
 
     // Material 3
     implementation("androidx.compose.material3:material3:1.2.1")
@@ -75,7 +73,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
