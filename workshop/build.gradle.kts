7b29542703096cfaf80893197a202eec7f5080fe
Черных Александр <razor01478920@mail.ru>
2024-05-23 20:47:14 +0300

    Обновил версию lifecycle-runtime-ktx, исправил версию библиотеки

--- workshop/build.gradle.kts
+++ workshop/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.ravenzip.workshop"
             artifactId = "workshop"
-            version = "1.5.0"
+            version = "1.5.2"
 
             afterEvaluate { from(components["release"]) }
         }
@@ -53,7 +53,7 @@ publishing {
 dependencies {
     // Kotlin
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
 
@@ -69,7 +69,7 @@ dependencies {
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    // Other
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
