1074bcba7909a3ec9e59bf573c6422d4a05475e6
Черных Александр <razor01478920@mail.ru>
2024-12-07 14:33:00 +0300

    Добавил класс Validator для описания базовых возможных валидаторов

---
+++ workshop/src/main/java/com/ravenzip/workshop/forms/Validator.kt
@@ -0,0 +1,24 @@
+package com.ravenzip.workshop.forms
+
+import android.util.Patterns.EMAIL_ADDRESS
+
+/** Возможные валидаторы для компонентов */
+class Validator {
+    companion object {
+        val required = { value: String ->
+            if (value.isEmpty()) "Поле обязательно для заполнения" else null
+        }
+
+        val minLength = { value: String, min: Int ->
+            if (value.length < min) "Минимальная длина $min символа" else null
+        }
+
+        val maxLength = { value: String, max: Int ->
+            if (value.length > max) "Максимальная длина $max символа" else null
+        }
+
+        val email = { value: String ->
+            if (!EMAIL_ADDRESS.matcher(value).matches()) "Введен некорректный email" else null
+        }
+    }
+}
