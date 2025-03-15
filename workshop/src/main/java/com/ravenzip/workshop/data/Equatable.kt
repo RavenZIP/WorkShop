2ece2ca8cabb9ece328b617366b0b6d6eef7a927
Черных Александр <razor01478920@mail.ru>
2025-03-15 12:10:49 +0300

    Тудушка

--- workshop/src/main/java/com/ravenzip/workshop/data/Equatable.kt
+++ workshop/src/main/java/com/ravenzip/workshop/data/Equatable.kt
@@ -3,6 +3,7 @@ package com.ravenzip.workshop.data
 import androidx.compose.runtime.Immutable
 
 @Immutable
+//TODO доработать его использование, потому что если кто-то захочет сделать лист со строками или числами, то будет проблема
 interface Equatable {
     override fun equals(other: Any?): Boolean
 }
