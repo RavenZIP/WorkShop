8d0c1d2be4c5162d4538110e2c8f0d0fa4cf3f0b
Черных Александр <razor01478920@mail.ru>
2025-06-05 08:30:27 +0300

    Заставляем реализовать основные методы обхода контролов

--- workshop/src/main/java/com/ravenzip/workshop/forms/group/IFormGroup.kt
+++ workshop/src/main/java/com/ravenzip/workshop/forms/group/IFormGroup.kt
@@ -3,9 +3,9 @@ package com.ravenzip.workshop.forms.group
 import com.ravenzip.workshop.forms.control.base.AbstractFormControl
 
 interface IFormGroup {
-    fun getAllControls(): List<AbstractFormControl> = emptyList()
+    fun getAllControls(): List<AbstractFormControl>
 
-    fun getAllGroups(): List<IFormGroup> = emptyList()
+    fun getAllGroups(): List<IFormGroup>
 
     fun getAll(): List<AbstractFormControl> =
         getAllControls() + getAllGroups().flatMap { x -> x.getAllControls() }
