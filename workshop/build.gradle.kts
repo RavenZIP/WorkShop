88b0a53504e997443af8dde84af1501724d97311
Черных Александр <razor01478920@mail.ru>
2025-06-10 23:06:58 +0300

    Актуализировал версию проекта (2.6.1)

--- workshop/build.gradle.kts
+++ workshop/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.ravenzip.workshop"
             artifactId = "workshop"
-            version = "2.6.0"
+            version = "2.6.1"
 
             afterEvaluate { from(components["release"]) }
         }
